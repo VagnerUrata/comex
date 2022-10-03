a6a6df1e07b412b9c2bbf5ad7165e1f674ade689
VagnerUrata <vagnerurata@gmail.com>
2022-10-03 18:55:16 -0300

    ajustes a serem feitos

---
+++ src/main/java/br/com/alura/comex/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,27 @@
+//package br.com.alura.comex.config.security;
+//
+//import br.com.alura.comex.repository.UsuarioRepository;
+//import org.springframework.security.core.token.TokenService;
+//
+//import javax.servlet.*;
+//import java.io.IOException;
+//
+//public class AutenticacaoViaTokenFilter implements Filter {
+//    public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+//    }
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//    }git stat
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+//}
--- src/main/java/br/com/alura/comex/modelo/Usuario.java
+++ src/main/java/br/com/alura/comex/modelo/Usuario.java
@@ -1,10 +1,20 @@
 package br.com.alura.comex.modelo;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +22,71 @@ public class Usuario {
 
     private String email;
     private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis = new ArrayList<>();
+
+    public Usuario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.perfis;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
